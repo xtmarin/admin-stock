c0c54310f5e8834da923592d022682870cf3d064
xtmarin <em20071404@gmail.com>
2026-03-14 13:11:22 -0500

    proyecto.next.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,21 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        ring: "hsl(var(--ring))",
+
+        primary: "hsl(var(--primary))",
+        "primary-foreground": "hsl(var(--primary-foreground))",
+
+        secondary: "hsl(var(--secondary))",
+        "secondary-foreground": "hsl(var(--secondary-foreground))",
+      },
+    },
   },
   plugins: [],
 }
-
